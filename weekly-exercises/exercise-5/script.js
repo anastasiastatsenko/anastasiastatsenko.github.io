0c1122ad162b9941933512420927362fb1629a4e
anastasiastatsenko <157340956+anastasiastatsenko@users.noreply.github.com>
2024-02-19 19:30:03 -0500

    .

--- weekly-exercises/exercise-5/script.js
+++ weekly-exercises/exercise-5/script.js
@@ -1,2 +1,10 @@
-console.log('hi there');
-console.log(THREE);
+var myAnimation = new hoverEffect ({
+    parent: document.querySelector('.image-gallery'),
+    intensity: 1,
+    image1: 'images/adams.jpg',
+    image2: 'images/adams1.jpg',
+    displacementImage: 'images/disp1.jpg',
+    speedIn: 2,
+    speedOut: 2
+});
+
